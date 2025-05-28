683778d01c78cb8b8f15c4e34a443d05c1254c6e
jefersondsgomes <jefersondsgomes@outlook.com>
2025-05-28 20:08:07 -0300

    feat: add database infrastructure

---
+++ src/Infrastructure/Extensions/InfrastructureExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Infrastructure.Connections;
+using Infrastructure.Connections.Interfaces;
+using Infrastructure.Exceptions;
+using Infrastructure.Factories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class InfrastructureExtensions
+{
+    const string MONGO_CONNECTION_STRING_VARIABLE_KEY = "MongoConnectionString";
+    const string APP_NAME_VARIABLE_KEY = "AppName";
+    const string DEFAULT_CLUSTER_NAME = "default";
+
+    public static IServiceCollection InjectInfrastructureDependencies(this IServiceCollection services)
+    {
+        services
+            .AddConnections();
+
+        return services;
+    }
+
+    private static IServiceCollection AddConnections(this IServiceCollection services)
+    {
+        var mongoConnectionString = Environment.GetEnvironmentVariable(MONGO_CONNECTION_STRING_VARIABLE_KEY);
+
+        EnvironmentVariableNotFoundException.ThrowIfIsNullOrWhiteSpace(mongoConnectionString, MONGO_CONNECTION_STRING_VARIABLE_KEY);
+
+        var appName = Environment.GetEnvironmentVariable(APP_NAME_VARIABLE_KEY);
+
+        EnvironmentVariableNotFoundException.ThrowIfIsNullOrWhiteSpace(appName, APP_NAME_VARIABLE_KEY);
+
+        var connection = new MongoConnection(DEFAULT_CLUSTER_NAME, mongoConnectionString!, appName);
+
+        services
+            .AddSingleton<IMongoConnection>()
+            .AddSingleton(MongoDataContextFactory.Create);
+
+        return services;
+    }
+}
