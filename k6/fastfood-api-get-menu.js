a8f964171e4f4d1668d131cf339dc4b9dbed10fc
jefersondsgomes <jefersondsgomes@outlook.com>
2025-07-28 17:39:11 -0300

    test: add k6 scripts

--- k6/fastfood-api-get-menu.js
+++ k6/fastfood-api-get-menu.js
@@ -1,18 +1,18 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
-const VUS_UP_STAGE_DURATION = '30s';
-const VUS_DOWN_STAGE_DURATION = '1m';
-const RAMP_UP_TARGET_VUS = 300;
+const VUS_UP_STAGE_DURATION = '40s';
+const VUS_DOWN_STAGE_DURATION = '30s';
+const RAMP_UP_TARGET_VUS = 100;
 const RAMP_DOWN_TARGET_VUS = 10;
 const BASE_URL = 'http://localhost:30080';
 
 export const options = {
-    startVUs: 50,
+    startVUs: 20,
+    gracefulRampDown: '20s',
     stages: [
         { duration: VUS_UP_STAGE_DURATION, target: RAMP_UP_TARGET_VUS },
-        { duration: VUS_DOWN_STAGE_DURATION, target: RAMP_DOWN_TARGET_VUS },
-        { duration: '30s', target: RAMP_DOWN_TARGET_VUS },
+        { duration: VUS_DOWN_STAGE_DURATION, target: RAMP_DOWN_TARGET_VUS }        
     ],
     thresholds: {
         http_req_duration: ['p(95)<200'],
